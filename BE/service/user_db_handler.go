364c3ad0c2499ca664a8dd50d2bb34a32f1dc89e
ThanhHoang <thanhhoang22032002@gmail.com>
2024-03-30 01:00:54 +0700

    update user_db_handler.go file

--- BE/service/user_db_handler.go
+++ BE/service/user_db_handler.go
@@ -9,7 +9,7 @@ import (
 func (sql *sql) SignUp(ctx context.Context, data *model.Users) (*model.Users, error) {
 	fmt.Printf("sql: %v\n", sql)
 	fmt.Printf("sql.db: %v\n", sql.db)
-	if err := sql.db.Table("users").FirstOrCreate(data).Error; err != nil {
+	if err := sql.db.Table("users").FirstOrCreate(&data, &model.Users{Email: data.Email}).Error; err != nil {
 		return nil, err
 	}
 	return data, nil
